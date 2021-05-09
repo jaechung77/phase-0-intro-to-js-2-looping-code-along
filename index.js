429fc316499385f094e654b4f09f69671f8ed5a4
jaechung77 <jaechung77@gmail.com>
2021-05-09 21:56:50 +1000

    looping

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function countDown(count){
+    for (let i=count; i >=0; i--){
+        console.log(i);
+    }
+}
+
+function writeCards(name, adjective){
+    const string = [];
+    for (let i=0; i<name.length; i++){
+        string[i]=  `Thank you, ${name[i]}, for the wonderful ${adjective} gift!`;
+    }
+    return string;  
+}
